0fd37dc8eae63171dc74459422ef795bae583fae
[Richard Sabando] <richardsabando@gmail.com>
2019-04-04 13:08:02 -0400

    added new wp folder/theme

---
+++ wp/web-journey/header.php
@@ -0,0 +1,41 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Web Dev Journey | The best place for Aspiring Developer Resources and knowledge</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?php echo get_bloginfo('template_directory')?>/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+    integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+
+</head>
+
+<body <?php body_class(); ?>>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg ">
+    <a class="navbar-brand" href="#">Web Dev Journey</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"><i class="fa fa-bars"></i></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Aspiring Dev</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Current Dev</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Blog</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
---
+++ wp/web-journey/index.php
@@ -0,0 +1,91 @@
+<?php get_header(); ?>
+  
+  <div class="jumbotron hero text-center ">
+    <div class="container">
+      <h1>LOOKING TO BECOME A WEB DEVELOPER?</h1>
+      <p class="lead p-5">This the best resource guide and info center to go to if you are looking to get into the
+        industry
+        or
+        are a current jr dev and looking to increase your skills. This center is made by a jr. developer for new and
+        aspiring developers.
+        Select a choice below and dive deeper into the information you need to help you on your path for change! You can
+        change careers and find your path to a better life!</p>
+      <div class="row text-center">
+        <div class="col-md-6 p-4">
+          <a href="#" class="btn color-btn btn-lg">Aspiring Dev</a>
+        </div>
+        <div class="col-md-6 p-4">
+          <a href="#" class="btn dark-btn btn-lg">Current Dev</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <section class="section-light">
+    <div class="container text-center">
+      <h2><i class="fas fa-code fa-2x color-primary"></i></h2>
+      <h2 class="p-3">Become a Developer and change your life!</h2>
+
+      <p class="lead">It is time for a change! Tired of the same old job? Tired of the the day in and day out grind in a
+        workplace where no one seems to care about you or what you do? Tired of working hard with little payout? You
+        might want to look into changing careers to the tech industry. One of the most in demand jobs in the tech
+        industry is the position of a <strong>Web Developer!</strong></p>
+      <p class="lead">According to the Bureau of Lobor and Statistics employment of web developers is projected to grow
+        15 percent
+        from now to 2026! Way faster than the average for all occupations out there! There are more vacant web
+        developer positions out there than can be filled. On top of that, the average salary for an entry level
+        developer is around $35,000 to as much as $70,000! The barrier of entry also requires <strong>no
+          degree!</strong> Want to learn more? Click the link below to start looking at resources that best suit your
+        learning style! </p>
+      <a href="#" class="btn color-btn btn-lg m-top-small">View Resources</a>
+    </div>
+  </section>
+  <section class="section-primary">
+    <h2 class="color-white text-center p-4">Benefits of Being a Developer</h2>
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-md-4">
+          <div class="card-bg box">
+            <i class="fas fa-scroll fa-3x color-primary"></i>
+            <h3>No Degree Required!</h3>
+            <hr class="underline">
+            <p>No degree? No problem! If you are wondering if you need a degree to get into the web development field
+              the answer is NO! All it takes is hard work and dedication! Skill is what matters in this industry. </p>
+
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card-bg box">
+            <i class="fas fa-money-check-alt fa-3x color-primary"></i>
+            <h3>High Average Salary!</h3>
+            <hr class="underline">
+            <p>So no degree? What about the pay? The answer is quite surprising. The average pay for web developers is
+              $76,000 per year according to indeed. This can vary depending on location and level of expertise. A high
+              Salary
+              is just one of the many benefits of becoming a developer.</p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="card-bg box">
+            <i class="fas fa-chart-line fa-3x color-primary"></i>
+            <h3>Limitless Potential</h3>
+            <hr class="underline">
+            <p>Companies like Amazon, Facebook, Google and Instagram (to name a few) became successful by achieving
+              success on the internet. You too can make your dreams a reality and make your own business or web
+              application that changes millions of people's lives for generations to come! </p>
+          </div>
+        </div>
+      </div> <!--   end row -->
+      <div class="bottom-btn-row">
+        <a href="#" class="btn color-btn btn-lg m-top-small">View Resources</a>
+      </div>
+    </div> <!--   end container -->
+  </section>
+  <section class="section-light text-center">
+    <div class="container">
+    <h2 class="p-3">Questions? Comments?</h2>
+      <p class="p-3">If you have any questions or want to share your web developer journey story feel free to use the form below to contact us. We will get back to you in a timely fashion. Best of luck on your journey to a better career!</p>
+    </div>
+  </section>
+
+  <?php get_footer(); ?>
